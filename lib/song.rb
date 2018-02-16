374017d76d7abd22c8a675cc0a5d6756203a7d01
Jay Bednar <ironviralmedia@gmail.com>
2018-02-16 18:16:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,11 +15,7 @@ class Song
   end
 
   def artist_name=(name)
-	    if (self.artist.nil?)
-	      self.artist = Artist.new(name)
-	    else
-	      self.artist.name = name
-	    end
+	   Artist.find_or_create_by_name(name)
 	end
 
 
