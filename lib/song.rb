d1b3a9dc137084f7066d79eefdd66e085a0a4304
Jay Bednar <ironviralmedia@gmail.com>
2018-02-16 19:25:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   end
 
   def artist_name=(name)
-    binding.pry
+    # binding.pry
 	  # Artist.find_or_create_by_name(name)
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
